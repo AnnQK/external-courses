c9848cf6d57913e6d35377be2bd7c88ea3d8bbcc
Anna Kovalik <89069692+AnnQK@users.noreply.github.com>
2022-05-05 10:14:19 +0300

    Feat: solve library event task

--- src/library_task/app/script.js
+++ src/library_task/app/script.js
@@ -1,42 +1,117 @@
 // variables
+import { options, BOOK_URL, localBooks } from './modules/books_api.js';
+
 const booksContainer = document.querySelector('.section__books');
-const BOOK_URL = 'https://api.itbook.store/1.0/new';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const bookPlaceholder = '../assets/images/img/book-cover.png';
+
+// INPUTS VARIABLES
+const searchFilter = document.querySelector('.input-keywords');
+const inputBookTitle = document.querySelector('.modal__form__input-title');
+const inputBookAuthor = document.querySelector('.modal__form__input-author');
+const inputBookCover = document.querySelector('.modal__form__input-cover');
+
+// BUTTONS VARIABLES
+const addBookBtn = document.querySelector('.add-book-btn');
+const closeModalBtn = document.querySelector('.modal__btn-close');
+const submitBookBtn = document.querySelector('.modal__btn-submit');
+const filterBtnContainer = document.querySelector('.filter-btns');
+
+// books on display
+const displayBooks = [];
 
 // WORK WITH API
 // getting books from api
 async function getBooks() {
-  const response = await fetch(BOOK_URL);
-  const data = await response.json();
-  const books = data.books;
-  return books;
+  try {
+    const response = await fetch(BOOK_URL, options);
+    const data = await response.json();
+    const books = data.Books;
+    await Promise.all(
+      books.map(async (book) => {
+        displayBooks.push({
+          title: book.title,
+          author: book.author,
+          image: book.imgUrl,
+          raiting: book.review.split(' ').join('')[0],
+        });
+      }),
+    );
+  } catch {
+    const books = localBooks;
+    books.map((book) => displayBooks.push({
+      title: book.title,
+      author: book.author,
+      image: book.imgUrl,
+      raiting: book.review.split(' ').join('')[0],
+    }));
+  }
+  return displayBooks;
 }
 
-// getting books authors
-async function getFullInfo(booksArr) {
-  const booksFull = [];
-  await Promise.all(
-    booksArr.map(async (item) => {
-      const response = await fetch(
-        `https://api.itbook.store/1.0/books/${item.isbn13}`,
-      );
-      const data = await response.json();
-      booksFull.push(data);
-    }),
-  );
-  return booksFull;
+// RATING STARS
+/* eslint-disable */
+function raiting(e) {
+  if (e.target.classList.contains('raiting-btn')) {
+    // find stars in clicked books container
+    const stars = Array.from(
+      e.target
+        .closest('.book-container__stats')
+        .querySelectorAll('.raiting-btn')
+    );
+
+    // stars container
+    const starsContainer = e.target.closest('.book-container__stats');
+    const clickedStarIdx = stars.indexOf(e.target);
+    starsContainer.dataset.raiting = clickedStarIdx + 1;
+    // find book in array
+    const containerTitle = Array.from(
+      starsContainer.parentElement.childNodes
+    )[3].textContent;
+    // update display books array
+    displayBooks.map((book) => {
+      if (book.title === containerTitle) {
+        return (book.raiting = starsContainer.dataset.raiting);
+      }
+    });
+  }
+  renderBooks(displayBooks);
 }
 
+function renderRaiting() {
+  const starsContainers = document.querySelectorAll('.book-container__stats');
+  Array.from(starsContainers).forEach((container) => {
+    const stars = container.querySelectorAll('.raiting-btn');
+    // check default raiting from api
+    const checkedIdx = container.dataset.raiting;
+    stars.forEach((item, i) => {
+      // remove active class from all stars
+      item.parentElement.classList.remove('active-raiting-btn');
+      // set this raiting to stars
+      if (i <= checkedIdx - 1) {
+        item.parentElement.classList.add('active-raiting-btn');
+      }
+    });
+  });
+  // event delegation
+  starsContainers.forEach((item) => {
+    item.addEventListener('click', raiting);
+  });
+}
+/* eslint-enable */
 // creating a container for every book
+
 function renderBooks(booksArr) {
-  return booksArr.forEach((book) => {
-    booksContainer.insertAdjacentHTML(
-      'beforeend',
-      `<div class="book-container">
+  booksContainer.innerHTML = '';
+  booksArr.map((book) => booksContainer.insertAdjacentHTML(
+    'afterBegin',
+    `<div class="book-container">
       <div class="image-box">
-      <img src="${book.image}" alt="" class="book-container__image" /></div>
-      <h3 class="book-container__title">${book.title.slice(0, 20)}…</h3>
-      <h4 class="book-container__author">${book.authors.split(',')[0]}</h4>
-      <div class="book-container__stats" data-raiting="0">
+      <img src="${book.image}" alt="${book.title}" class="book-container__image" /></div>
+      <h3 class="book-container__title">${book.title}</h3>
+      <h4 class="book-container__author">${book.author}</h4>
+      <div class="book-container__stats" data-raiting="${book.raiting}">
         <svg id="" class="svg-logo stats-star">
           <use href="#star" class="raiting-btn"></use>
         </svg>
@@ -53,42 +128,87 @@ function renderBooks(booksArr) {
           <use href="#star" class="raiting-btn"></use>
         </svg>
     </div>`,
-    );
-  });
+  ));
+  renderRaiting();
 }
 
-// RATING STARS
+// MODAL ADD BOOK
 
-function raiting(e) {
-  if (e.target.classList.contains('raiting-btn')) {
-    const stars = Array.from(
-      e.target
-        .closest('.book-container__stats')
-        .querySelectorAll('.raiting-btn'),
+function showModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  document.body.classList.add('stop-scroll');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+  document.body.classList.remove('stop-scroll');
+}
+
+function addBookInfo(e) {
+  e.preventDefault();
+  if (inputBookTitle.value === undefined || inputBookTitle.value === '') return;
+  const uploadImage = inputBookCover.files[0];
+  const imageURL = uploadImage === undefined || uploadImage == null
+    ? bookPlaceholder
+    : URL.createObjectURL(uploadImage);
+  const addedBook = {
+    title: inputBookTitle.value,
+    author: inputBookAuthor.value,
+    image: imageURL,
+  };
+  inputBookAuthor.value = '';
+  inputBookTitle.value = '';
+  displayBooks.push(addedBook);
+  renderBooks(displayBooks);
+  closeModal();
+}
+
+// SEARCH BAR
+
+function searchBook(e) {
+  const searchValue = e.target.value.toLowerCase();
+  const filteredBooks = displayBooks.filter(
+    (item) => item.title.toLowerCase().includes(searchValue)
+      || item.author.toLowerCase().includes(searchValue),
+  );
+  renderBooks(filteredBooks);
+}
+
+// SORTING BOOKS
+// sorting popular
+
+function popularSort() {
+  const filteredBooks = displayBooks.filter((item) => item.raiting === '5');
+  return renderBooks(filteredBooks);
+}
+
+// event delegation
+filterBtnContainer.addEventListener('click', (e) => {
+  if (e.target.dataset.func) {
+    const clickedBtn = e.target;
+    // change visual active btn
+    Array.from(filterBtnContainer.querySelectorAll('[data-func]')).forEach(
+      (item) => item.classList.remove('active-filter-btn'),
     );
-    // stars container
-    const starsContainer = e.target.closest('.book-container__stats');
-    starsContainer.classList.add('disable-stars');
-    const clickedStarIdx = stars.indexOf(e.target);
-    starsContainer.dataset.raiting = clickedStarIdx + 1;
-    stars.forEach((star, index) => {
-      if (index <= clickedStarIdx) {
-        star.parentElement.classList.add('active-raiting-btn');
-      }
-    });
+    clickedBtn.classList.add('active-filter-btn');
+
+    // sort popular
+    if (clickedBtn.dataset.func === 'popularSort') {
+      return popularSort();
+    }
+    // not popular sorting
+    return renderBooks(displayBooks);
   }
-}
+  return {};
+});
 
-function renderRaiting() {
-  // all raiting containers
-  const starsContainers = document.querySelectorAll('.book-container__stats');
-  // delegating click events
-  starsContainers.forEach((item) => {
-    item.addEventListener('click', raiting);
-  });
-}
+submitBookBtn.addEventListener('click', addBookInfo);
+searchFilter.addEventListener('input', searchBook);
+addBookBtn.addEventListener('click', showModal);
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+// popularSortBtn.addEventListener('click', popularSort);
 
-getBooks()
-  .then((res) => getFullInfo(res))
-  .then((res) => renderBooks(res))
-  .then(() => renderRaiting());
+getBooks().then((res) => renderBooks(res));
